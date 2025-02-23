9197b5a111b14036e5326d4f1716cc10d9ce49f2
abesanchez2003 <abe_sanchez@tamu.edu>
2025-02-23 17:45:15 -0600

    Added code that instantiates wifi module and connects esp32 chip to wifi with a given ssid and password.

--- Firmware/development_software/src/main.cpp
+++ Firmware/development_software/src/main.cpp
@@ -3,36 +3,109 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include <vector>
+#include <driver/ledc.h>
+#include <esp_err.h>
+#include <esp_log.h>
+#include <esp_wifi.h>
+#include <string>
+#include <cstring>
+#include <nvs_flash.h>
 
 #define LED_PIN GPIO_NUM_2  // ESP32 onboard LED
+#define POT_RES GPIO_NUM_35
+void delay()
+{
+    for (int i = 0; i < 1000000000; i++)
+    {
+
+    }
+}
+int connect_wifi(std:: string ssid, std:: string pwd){
+    //this function will be responsible for establishing wifi connectivity
+    // args(ssid, pwd) takes ssid or wifi network name and network password , 
+    static const char* TAG = "connect_wifi";
+    if(ssid.length() != 0 && pwd.length() != 0){
+
+        ESP_LOGD(TAG, "Attempting Wifi connection");
+
+        
+        wifi_init_config_t init_configuration = WIFI_INIT_CONFIG_DEFAULT();
+        
+         // setting default wifi initialization configuration
+        esp_err_t wifi_init = esp_wifi_init(&init_configuration); // calling wifi initilization and logging success or error
+        if (wifi_init == ESP_OK){
+            ESP_LOGD(TAG, "Wifi Initilized!");
+        }
+        else
+        {
+            ESP_LOGD(TAG, "WiFi Init Status: %s", esp_err_to_name(wifi_init));
+            return -1;
+
+        }
+
+        wifi_config_t wifi_credents = {};
+        strcpy((char*)wifi_credents.sta.ssid, ssid.c_str());
+        strcpy((char*)wifi_credents.sta.password, pwd.c_str());
+        ESP_LOGD(TAG,"WiFi SSID %s", wifi_credents.sta.ssid);
+        ESP_LOGD(TAG, "WiFi Password %s",wifi_credents.sta.password);
+
+        esp_err_t setCredentials = esp_wifi_set_config(WIFI_IF_STA, &wifi_credents); // setting wifi credentials
+
+        if(setCredentials == ESP_OK){
+            ESP_LOGD(TAG, "Credentials Set Successfully");
+        }
+        else
+        {
+            ESP_LOGD(TAG, "Credential Setting Status: %s", esp_err_to_name(setCredentials));
+        }
+        esp_err_t start_wifi = esp_wifi_start(); //starting wifi
+        if(start_wifi == ESP_OK){
+            ESP_LOGD(TAG, "Wifi Started successfully");
+
+        }
+        else
+        {
+            ESP_LOGD(TAG, "WiFi Start Status: %s", esp_err_to_name(start_wifi));
+        }
+        esp_wifi_set_mode(WIFI_MODE_STA);
+
+        esp_err_t wifi_connect = esp_wifi_connect(); // attmepting wifi connection 
+        if(wifi_connect == ESP_OK){
+            ESP_LOGD(TAG, "Wifi Connected Successfully");
+        }
+        else
+        {
+            ESP_LOGD(TAG, "Wifi Connect Status: %s", esp_err_to_name(wifi_connect));
+        }
+        return 0;
+
+    }
+    else
+    {
+        return -1;
+    }
+}
 
 extern "C" void app_main(void) {
 
-    // Configure GPIO2 as output
-    //gpio_reset_pin(LED_PIN);
-    //gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 
-   /* 
-   while (1) {
-        gpio_set_level(LED_PIN, 1);  // Turn LED ON
-        printf("LED ON\n");
-        vTaskDelay(pdMS_TO_TICKS(1000));  // Delay 1 second
-
-        gpio_set_level(LED_PIN, 0);  // Turn LED OFF
-        printf("LED OFF\n");
-        vTaskDelay(pdMS_TO_TICKS(1000));  // Delay 1 second
-    }
-        */
     volatile uint32_t *output = (volatile uint32_t *)(0x3FF44000 + 0x04);
     volatile uint32_t *enableoutput = (volatile uint32_t *)(0x3FF44000 + 0x20);
-    
+    std:: string ssid, pwd;
+    ssid = "fivegigas";
+    pwd = "twosimps";
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        nvs_flash_erase();  // ⚠️ If NVS is corrupted, erase and re-init
+        nvs_flash_init();
+    }
 
-    while(1){
+    int wifi = connect_wifi(ssid, pwd);
+    while(wifi == 0){
         *enableoutput = 1;
         *output = 1 << 0;
 
-    }
-    
+    } 
 
 
 
